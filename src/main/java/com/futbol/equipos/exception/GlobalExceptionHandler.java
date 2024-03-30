120269eb45b470c1e08bb250ed2ac097c47da82c
javiq <javierquinteros93@gmail.com>
2024-03-30 10:41:55 -0300

    Corrigiendo responses

--- src/main/java/com/futbol/equipos/exception/GlobalExceptionHandler.java
+++ src/main/java/com/futbol/equipos/exception/GlobalExceptionHandler.java
@@ -1,11 +1,12 @@
 package com.futbol.equipos.exception;
 
+import com.futbol.equipos.entity.ResponseObject;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.MalformedJwtException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,39 +20,44 @@ import java.util.NoSuchElementException;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-    public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("La solicitud es inválida");
+    public ResponseEntity<ResponseObject> handleValidationException(MethodArgumentNotValidException ex) {
+        ResponseObject response = new ResponseObject("La solicitud es inválida", HttpStatus.BAD_REQUEST.value());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
     @ExceptionHandler(NoSuchElementException.class)
-    @ResponseStatus(code = HttpStatus.NOT_FOUND)
-    public ResponseEntity<String> handleNoElementException(NoSuchElementException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Equipo no encontrado");
+    public ResponseEntity<ResponseObject> handleNoElementException(NoSuchElementException ex) {
+        ResponseObject response = new ResponseObject("Equipo no encontrado", HttpStatus.NOT_FOUND.value());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
 
     @ExceptionHandler(ExpiredJwtException.class)
-    public ResponseEntity<String> handleExpiredJwtException(ExpiredJwtException ex) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Token JWT expirado");
+    public ResponseEntity<ResponseObject> handleExpiredJwtException(ExpiredJwtException ex) {
+        ResponseObject response = new ResponseObject("Token JWT malformado", HttpStatus.BAD_REQUEST.value());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
     }
 
     @ExceptionHandler(MalformedJwtException.class)
-    public ResponseEntity<String> handleMalformedJwtException(MalformedJwtException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Token JWT malformado");
+    public ResponseEntity<ResponseObject> handleMalformedJwtException(MalformedJwtException ex) {
+        ResponseObject response = new ResponseObject("Token JWT malformado", HttpStatus.BAD_REQUEST.value());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
     @ExceptionHandler(SignatureException.class)
-    public ResponseEntity<String> handleSignatureException(SignatureException ex) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Error de firma del token JWT");
+    public ResponseEntity<ResponseObject> handleSignatureException(SignatureException ex) {
+        ResponseObject response = new ResponseObject("Error de firma del token JWT", HttpStatus.BAD_REQUEST.value());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
     }
 
     @ExceptionHandler(JwtException.class)
-    public ResponseEntity<String> handleJwtException(JwtException ex) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Error JWT: " + ex.getMessage());
+    public ResponseEntity<ResponseObject> handleJwtException(JwtException ex) {
+        ResponseObject response = new ResponseObject("Error JWT: " + ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
     }
 
     @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<String> handleAuthenticationException(AuthenticationException ex) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falló la autenticación");
+    public ResponseEntity<ResponseObject> handleAuthenticationException(AuthenticationException ex) {
+        ResponseObject response = new ResponseObject("Falló la autenticación", HttpStatus.BAD_REQUEST.value());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
     }
 }
